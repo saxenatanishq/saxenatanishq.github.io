b769256b774c525937565efacaa8388936d366b0
Tanishq <tanishqsaxena.in@gmail.com>
2025-06-20 03:13:46 +0530

    adding the resume to get it's github link

--- src/MyComps/Footer.js
+++ src/MyComps/Footer.js
@@ -13,7 +13,7 @@ const Footer = () => {
             <div class="max-w-7xl mx-auto px-6">
                 <p class="text-sm">Last updated on <span id="last-updated">June 19, 2025</span>. &copy; 2025.</p>
                 <p class="text-sm mt-2">
-                    Source code available on <a href="https://github.com/saxenatanishq/personal-webpage" target="_blank" class="text-indigo-400 hover:underline">GitHub</a>.  Contact me: <a href="mailto:tanishqsaxena.in@gmail.com" class="text-indigo-400 hover:underline">tanishqsaxena.in@gmail.com</a> 
+                    Source code available on <a href="https://github.com/saxenatanishq/saxenatanishq.github.io" target="_blank" class="text-indigo-400 hover:underline">GitHub</a>.  Contact me: <a href="mailto:tanishqsaxena.in@gmail.com" class="text-indigo-400 hover:underline">tanishqsaxena.in@gmail.com</a> 
                 </p>
             </div>
         </footer>
