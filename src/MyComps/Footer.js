1418a7d117c609da6ff635e70fb3a66c42bdde27
Tanishq <tanishqsaxena.in@gmail.com>
2025-07-11 11:26:45 +0530

    removing the auto update of dates

--- src/MyComps/Footer.js
+++ src/MyComps/Footer.js
@@ -1,13 +1,4 @@
-import React, {useEffect} from 'react'
-
 const Footer = () => {
-    useEffect(() => {
-        // Standard way to update the last updated date (So, we don't have to manually change it)
-        const last = document.getElementById('last-updated');
-        const today = new Date();
-        const options = { year: 'numeric', month: 'long', day: 'numeric' };
-        last.textContent = today.toLocaleDateString('en-US', options); 
-    });
     return (
         <footer class="bg-gray-800 text-white py-6 text-center mt-auto">
             <div class="max-w-7xl mx-auto px-6">
